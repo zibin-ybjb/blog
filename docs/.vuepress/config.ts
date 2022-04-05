f66af12dcded248051edbe5a533f15a9df45b0a6
zibin <liuzibin2021@126.com>
2022-04-05 16:15:46 +0800

    update: 更新路径配置

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -8,7 +8,7 @@ export default defineUserConfig<DefaultThemeOptions>({
   lang: 'zh-CN',
   title: '子斌的前端分享',
   description: 'Just playing around',
-  base:'/vuepress-blog/',
+  base:'/',
 
   // 主题和它的配置
   theme: '@vuepress/theme-default',
